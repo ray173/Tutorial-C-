b78b1f26f6b3270eeba302de3d8c8cad595caf62
ray173 <85490495+ray173@users.noreply.github.com>
2021-09-23 04:09:35 +0700

    Create Aritmatika.cpp

---
+++ Aritmatika.cpp
@@ -0,0 +1,17 @@
+//ARITMATIKA / INPUT (u just need to change the operator)
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    int result;
+
+    cout<<"input number :";
+    cin>>a;
+
+    cout<<"input number :";
+    cin>>b;
+
+    result=a+b;
+    cout<<"result of "<< a << " + " << b << " = " << result;
+}
