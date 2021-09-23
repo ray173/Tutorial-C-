b6ad924b9ae064aab0a8eb405432f55693829fcc
Sihab <85490495+ray173@users.noreply.github.com>
2021-09-24 05:04:58 +0700

    Create Do while loop.cpp

---
+++ Do while loop.cpp
@@ -0,0 +1,13 @@
+DO WHILE LOOP
+
+#include<iostream>
+using namespace std;
+int main( ){
+    int a = 3;
+    do{
+        cout<<"hore ";
+        cout<< a << endl;
+        a++;
+    }while(a < 10);
+
+}
