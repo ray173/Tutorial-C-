bad3f2809004f03a8ad04c18c6239b6df8bab464
Sihab <85490495+ray173@users.noreply.github.com>
2021-09-24 04:17:49 +0700

    Create Calculate area of triangle.cpp

---
+++ Calculate area of triangle.cpp
@@ -0,0 +1,28 @@
+//CALCULATE AREABOF TRIANGLE
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+    double l, b, h;
+    cout<<"CALCULATE AREA OF TRIANGLE"<<endl;
+    
+    cout<<"Input Base :";
+    cin>>b;
+    
+    cout<<"Input Height :";
+    cin>>h;
+    
+    l = (b*h)/2;
+    
+    cout<<endl;
+    cout<<"RESULT"<<endl;
+    cout<<endl;
+    cout<<"Base :"<< b << "cm"<<endl;
+    cout<<"Height :"<<h<<"cm"<<endl;
+    cout<<endl;
+    cout<<"answer : "<<endl;
+    cout<<"L = (b x h)/2 L = ("<<b<<"x"<<h<<")/2"<<endl;
+cout<<"L = "<<l<<"cm";
+
+}
