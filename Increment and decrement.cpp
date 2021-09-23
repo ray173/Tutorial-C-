264ef71ce5879774d3747f4251afa5b7ed192db5
Sihab <85490495+ray173@users.noreply.github.com>
2021-09-24 05:03:29 +0700

    Create Increment and decrement.cpp

---
+++ Increment and decrement.cpp
@@ -0,0 +1,20 @@
+//INCREMET DAN DECREMENT
+
+#include<iostream>
+using namespace std;
+int main( ){
+    int a=5;
+    int b=5;
+    
+    //postincrement
+    cout<< a << endl;
+    cout<< a++ <<endl;
+    cout<< a << endl;
+    
+    cout<<endl;
+    
+    //preincrement
+    cout<< b << endl;
+    cout<< ++b <<endl;
+    cout<< b << endl;
+}
