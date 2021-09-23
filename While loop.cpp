0c15abc33a8426d32620a31a3b538761ae612749
Sihab <85490495+ray173@users.noreply.github.com>
2021-09-24 05:04:21 +0700

    Create While loop.cpp

---
+++ While loop.cpp
@@ -0,0 +1,13 @@
+//WHILE LOOP
+
+#include<iostream>
+using namespace std;
+int main( ){
+    int a = 5;
+    while (a<20){
+        cout<<"hore ";
+        cout<< a <<endl;
+        a+=3;
+    }
+    cout<<"selesai"<<endl;
+}
