746e189c0b164e17c52e10e3da4aa69245fa0dd0
Sihab <85490495+ray173@users.noreply.github.com>
2021-09-24 04:43:52 +0700

    Create Operator assigment.cpp

---
+++ Operator assigment.cpp
@@ -0,0 +1,21 @@
+//OPERATOR ASSIGNMENT
+
+#include<iostream>
+using namespace std;
+int main(){
+    int num = 10;
+ 
+    cout<<"first number :"<<num<<endl;
+    //operator assignment
+    //num = num+5;
+    num += 5;
+    cout<<"num + 5 = "<<num<<endl;
+    num -= 5;
+    cout<<"num + 5 = "<<num<<endl;
+    num *= 5;
+    cout<<"num + 5 = "<<num<<endl;
+    num /= 5;
+    cout<<num + 5 = "<<num<<endl;
+    num %= 5;
+    cout<<"num + 5 = "<<num<<endl;
+}
