6c61277b24516b866ce24c423106baf2b4ecfc10
ray173 <85490495+ray173@users.noreply.github.com>
2021-09-23 14:18:02 +0700

    Create ood and even number.cpp

---
+++ ood and even number.cpp
@@ -0,0 +1,12 @@
+//FIND ODD NUMBER AND EVEN NUMBER
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a, result;
+    cout<<"input number :";
+    cin>>a;
+    hasil = a%2;
+    string type = result == 1 ? "Ganjil (ood)" : "Genap(even)";
+    cout<<"number "<< a << " is "<<type<<endl;
+}
