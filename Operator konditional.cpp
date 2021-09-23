eb5c2da6a6a5cbce6f7f8b2fe4620d6086419a44
Sihab <85490495+ray173@users.noreply.github.com>
2021-09-24 04:30:38 +0700

    Create Operator konditional.cpp

---
+++ Operator konditional.cpp
@@ -0,0 +1,16 @@
+//OPERATOR KONDITIONAL
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   int a, b;
+    bool result;
+    cout<<"Input score :";
+    cin>>a;
+    
+    b = 59;
+    hasil = a > b;
+    string konditional = result == 1 ?"Graduated":"No Graduated";
+    cout<<"result = " << konditional <<endl;
+}
