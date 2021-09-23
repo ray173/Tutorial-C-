a471b6ab7159f0ec29205ac985605468a3f0b10e
Sihab <85490495+ray173@users.noreply.github.com>
2021-09-24 05:07:19 +0700

    Create For using break and continue.cpp

---
+++ For using break and continue.cpp
@@ -0,0 +1,13 @@
+//FOR MENGGUNAKAN BREAK DAN CONTINUE
+
+#include<iostream>
+using namespace std;
+int main(){
+    for (int i = 0; i<= 10; i++){
+        if(i == 5){
+            break;
+            //continue;
+        }
+        cout<< i << endl;
+    }
+}
