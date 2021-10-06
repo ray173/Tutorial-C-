9258bb99f6e8abb914371d7ee5b4ef5c8eb6ead1
Sihab <85490495+ray173@users.noreply.github.com>
2021-10-07 05:57:16 +0700

    Create Fibonacci.cpp

---
+++ Fibonacci.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //f_n = f_n1 + f_n2
+    int n;
+    int f_n;
+    int f_n1;
+    int f_n2;
+    
+    cout<<"Program deret fibonacci"<<endl;
+    cout<<"masukkan nilai ke-n :";
+    cin>> n;
+    
+    f_n1 = 1;
+    f_n2 = 0;
+    f_n = f_n1 + f_n2;
+    cout << f_n <<" ";
+    for(int i = 1; i < n; i++){
+        f_n = f_n1 + f_n2;
+        f_n2 = f_n1;
+        f_n1 = f_n;
+        cout<< f_n <<" ";
+    }
+    
+    cin.get();
+    return 0;
+}
