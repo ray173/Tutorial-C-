2e66631c5855338cbbf86b8568c130ca84f6b58a
Sihab <85490495+ray173@users.noreply.github.com>
2021-10-12 05:18:46 +0700

    Update Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -2,12 +2,37 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"masukkan jumlah pola :";
+    cout<<"masukkan jumblah pola :";
     cin>>n;
+    
+    cout<<"pola 1\n"<<endl;
+    
     for(int i=1; i<=n; i++){
         for(int j=1; j<=i; j++){
             cout<<"#";
         }
         cout<<endl;
     }
-}
+    
+    cout<<"pola 2\n"<<endl;
+    
+    for(int i=1; i<=n; i++){
+        for(int j=n; j>=i; j--){
+            cout<<"#";
+        }
+        cout<<endl;
+    }
+    
+    cout<<"pola 3\n"<<endl;
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<i; j++){
+            cout<<" ";
+        }
+        for(int k=n; k>=i; k--){
+            cout<<"#";
+        }
+        cout<<endl;
+    }
+    
+} 
