f5f32c6a5492256e01a9ef626472f0a213c89094
Sihab <85490495+ray173@users.noreply.github.com>
2021-12-20 01:33:00 +0700

    Update Array+for.cpp

--- Array+for.cpp
+++ Array+for.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
 	int n;
-
+	int jumlah = 0, terkecil, terbesar;
 	cout<<"input banyak nilai : ";
 	cin>>n;
 
@@ -12,9 +12,27 @@ int main(){
 	for(int i = 0; i<n; i++){
 		cout<<"inputkan nilai ke-"<< i+1<<": ";
 		cin>>nilai[i];
+		jumlah = jumlah + nilai[i];
+
+		if(i==0){
+			terkecil = nilai[0];
+			terbesar = nilai[0];
+		}
+		if(nilai[i] < terkecil){
+			terkecil = nilai[i];
+		}
+
+		if(nilai[i] > terbesar){
+			terbesar = nilai[i];
+		}
+
 	}
 
 	for(int i = 0; i<n; i++){
-		cout<<"nilai ke-"<< i+1 <<":" <<nilai [i] <<endl;
+		cout<<"nilai ke-"<< i+1 <<": " << nilai[i] <<endl;
+		
 	}
+	cout<<"jumlah nilai adalah : "<< jumlah<<endl;
+	cout<<"nilai terbesar adalah : "<<terbesar<<endl;
+	cout<<"nilai terkecil adalah : "<<terkecil<<endl;
 }
