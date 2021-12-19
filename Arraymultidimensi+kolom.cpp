13ebb3e0c35c9b08fc16b7463a436667f393bb88
Sihab <85490495+ray173@users.noreply.github.com>
2021-12-20 02:16:49 +0700

    Create Arraymultidimensi+kolom.cpp

---
+++ Arraymultidimensi+kolom.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	
+	int a[2][3];
+
+	for(int i = 0; i<=1; i++){
+		for(int j = 0; j<=2; j++){
+			cout<<"inputkan nilai index ke-["<<i<<"]["<<j<<"]: ";
+			cin>>a[i][j];
+		}
+	}	
+
+
+	
+	//mencetak isi array
+
+	for(int i = 0; i<=1; i++){
+		cout<<endl;
+		for(int j = 0; j<=2; j++){
+			cout<<a[i][j]<<" ";		
+		}
+	}
+
+}
