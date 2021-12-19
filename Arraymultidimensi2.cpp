8cc79e532fc0f93af0430df349f90b0163857b13
Sihab <85490495+ray173@users.noreply.github.com>
2021-12-20 01:56:29 +0700

    Create Arraymultidimensi2.cpp

---
+++ Arraymultidimensi2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	//2 baris dan 3 kolom
+	int a[2][3] = {{5,10,15}, {20,25,30}};
+
+
+	
+	//mencetak isi array
+	cout<<"Nilai index ke-[0][0] : "<<a[0][0]<<endl;
+	cout<<"Nilai index ke-[0][1] : "<<a[0][1]<<endl;
+	cout<<"Nilai index ke-[0][2] : "<<a[0][2]<<endl;
+	cout<<"Nilai index ke-[1][0] : "<<a[1][0]<<endl;
+	cout<<"Nilai index ke-[1][1] : "<<a[1][1]<<endl;
+	cout<<"Nilai index ke-[1][2] : "<<a[1][2]<<endl;
+
+}
